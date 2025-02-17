6b9093392a69ebf996f3f252c59e5e3260d4cbc8
Adam Dasovich <adamdasovich@gmail.com>
2025-02-17 08:59:50 -0500

    add_item was implemented

--- slut_api/serializers.py
+++ slut_api/serializers.py
@@ -1,7 +1,32 @@
 from rest_framework import serializers
-from .models import Product
+from .models import *
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'name', 'slug', 'image', 'description', 'category', 'price']
+
+
+class DetailedProductsSerializer(serializers.ModelSerializer):
+    similar_products = serializers.SerializerMethodField()
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'slug', 'image', 'description', 'category', 'price', 'similar_products']
+
+    def get_similar_products(self, product):
+        products = Product.objects.filter(category=product.category).exclude(id=product.id)
+        serializer = ProductSerializer(products, many=True)
+        return serializer.data
+    
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = ['id', 'cart_code', 'created_at', 'modified_at']
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+    cart = CartSerializer(read_only=True)
+    class Meta:
+        model = CartItem
+        fields = ['id', 'cart', 'product', 'quantity']
